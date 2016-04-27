c3b67869224c1a9bcfa0160bc2b63397d4a3aca2
john <munn.jon@gmail.com>
2016-04-27 03:36:16 -0300

    add script.json and converted MultieWhisper.js to use a namespace

--- MultiWhisper/MultiWhisper.js
+++ MultiWhisper/MultiWhisper.js
@@ -1,32 +1,35 @@
+var multiWhisper = multiWhisper || {};
+
 var playerArray = [];
 var message = "";
 
-on('chat:message', function(msg){
-    if(msg.content.substring(0, 2).toLowerCase() == "!w"){
-        var content = msg.content.replace(/@/g, '/@').replace('^', '/^');
+multiWhisper.init=function(msg){
     
-        var msgArray = content.split("/")
-        for(var i = 0; i<msgArray.length; i++){
-            if(msgArray[i].indexOf("@") !== -1){
-                playerArray.push(msgArray[i].replace('@','').slice(0, -1));
-            } else if (msgArray[i].indexOf("^") !== -1){
-                message = msgArray[i].replace('^', '')
-            }
+    var content = msg.content.replace(/@/g, '/@').replace('^', '/^');
+    var msgArray = content.split("/")
+    
+    for(var i = 0; i<msgArray.length; i++){
+        if(msgArray[i].indexOf("@") !== -1){
+            playerArray.push(msgArray[i].replace('@','').slice(0, -1));
+        } else if (msgArray[i].indexOf("^") !== -1){
+            message = msgArray[i].replace('^', '');
         }
-        
-        var players = findObjs({_type: 'player'});
-        players.forEach(function(player) {
-            comparePlayerArray(player)
-        });
-        
-        checkIfNotFound();
-        
-        playerArray = [];
-        sendChat(msg.who, message);
     }
-});
 
-function comparePlayerArray(player){
+    this.findPlayers();
+    this.checkIfNotFound();
+    
+    sendChat(msg.who, message);
+}
+
+multiWhisper.findPlayers= function(){
+    var players = findObjs({_type: 'player'});
+    players.forEach((player) => {
+        this.comparePlayerArray(player);
+    });
+}
+
+multiWhisper.comparePlayerArray=function(player){
     for(var p = 0; p<playerArray.length; p++){
         var displayName = player.get('_displayname').toLowerCase();
         if(displayName == playerArray[p].toLowerCase()){
@@ -36,13 +39,18 @@ function comparePlayerArray(player){
     }
 }
 
-function checkIfNotFound(){
+multiWhisper.checkIfNotFound=function(){
     var nameNotFound = "";
     if(playerArray.length>1){
         for(var p = 0; p<playerArray.length; p++){
             nameNotFound = nameNotFound + playerArray[p] + ", ";
         }
-        
         message = message + "\n\nThe following players where not Found: " + nameNotFound;
     }
 }
+
+on('chat:message', function(msg){
+    if(msg.content.substring(0, 2).toLowerCase() == "!w"){
+        multiWhisper.init(msg); 
+    }
+});
